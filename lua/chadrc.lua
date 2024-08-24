503ca13a4f63853573e09aeb707029f398a70191
Paul Yoon <pauljinyoon@gmail.com>
2024-08-24 15:34:25 -0700

    fixed linenumber visibility

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,26 +1,29 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui= {
-  theme = "vscode_dark",
-  transparency = true,
+M.ui = {
+    theme = "vscode_dark",
+    transparency = true,
 
-	hl_override = {
-    CursorLine = {
-      bg = "one_bg",
+    hl_override = {
+        CursorLine = {
+            bg = "one_bg",
+        },
+        LineNr = {
+            bg = "one_bg"
+        }
+    },
+    cmp = {
+        icons = true,
+        lspkind_text = true,
+        style = "default",
+    },
+    statusline = {
+        theme = "vscode_colored"
     }
-	},
-  cmp = {
-    icons = true,
-    lspkind_text = true,
-    style = "default",
-  },
-  statusline = {
-    theme = "vscode_colored"
-  }
 }
 
 return M
