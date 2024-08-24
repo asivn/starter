fa9b08d58d0835c04330b4274d4ec602e9288567
Paul Yoon <pauljinyoon@gmail.com>
2024-08-24 15:36:34 -0700

    fixed vimtex opts

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,13 +50,14 @@ return {
     },
     {
         "lervag/vimtex",
+        lazy = false,
         init = function()
-            vim.g['vimtex_view_method'] = 'Preview'
+            vim.g['vimtex_view_method'] = 'general'
             vim.g['vimtex_quickfix_mode'] = 0 -- suppress error reporting on save and build
             vim.g['tex_conceal'] = 'mgs'
             vim.g['vimtex_matchparen_enabled'] = 0
             vim.g['vimtex_delim_stopline'] = 1
-            vim.o.conceallevel = 1
+            vim.o.conceallevel = 2
             vim.g['vimtex_log_ignore'] = ({ -- Error suppression:
                 'Underfull',
                 'Overfull',
