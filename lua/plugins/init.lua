61dd29eeb4148a3ff52ed85d7e9ad7c17f638904
Paul Yoon <pauljinyoon@gmail.com>
2024-09-02 22:25:07 -0700

    cmdline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -226,4 +226,46 @@ return {
             },
         },
     },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("noice").setup {
+                -- add any options here
+                lsp = {
+                    signature = {
+                        enabled = false,
+                    }
+                },
+                routes = {
+                    {
+                        filter = {
+                            event = "msg_show",
+                            any = {
+                                { find = "%d+L, %d+B" },
+                                { find = "; after #%d+" },
+                                { find = "; before #%d+" },
+                                { find = "%d fewer lines" },
+                                { find = "%d more lines" },
+                            },
+                        },
+                        opts = { skip = true },
+                    },
+                },
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            }
+        end,
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   "rcarriga/nvim-notify",
+        },
+    },
 }
